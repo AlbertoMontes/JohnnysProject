ce8d3becc54fa445014e353297c00793dfbce01d
Alberto Montes <amfullsail@fullsail.edu>
2016-08-20 02:44:02 -0400

    Debugged/Altered

---
+++ app/src/main/java/com/amontes/johnnysproject/MainActivity.java
@@ -0,0 +1,85 @@
+package com.amontes.johnnysproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // Class member variables.
+    //private TextView quantity;  <--We don't need to reference this static View object.
+    private TextView quantityTextView;
+    private TextView priceTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instantiate these once. It probably works the way you had it, but why create a new instance every time a method is fired????
+        //quantity = (TextView) findViewById(R.id.quantity_text_view); <-- Again, no need for this.
+        quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        priceTextView = (TextView) findViewById(R.id.price_text_view);
+
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        // Just grab the current quantity.
+        //int quantity = 5;
+        //display(quantity);
+        int quantity = Integer.parseInt(quantityTextView.getText().toString());
+        displayPrice(quantity * 5);
+    }
+
+    // This method is called when the + button is clicked
+    public void increment(View view){
+        /* read quantity text view
+            assign to variable
+            variable++
+            display(variable)
+         */
+        int newQuantity = Integer.parseInt(quantityTextView.getText().toString());
+        newQuantity++;
+        display(newQuantity);
+    }
+
+    // This method is called when the - button is clicked
+    public void decrement(View view){
+        /* read quantity text view
+            assign to variable
+            if > 0 variable--
+            display(variable)
+         */
+        int newQuantity = Integer.parseInt(quantityTextView.getText().toString());
+        if (newQuantity > 0) {
+            newQuantity--;
+        }
+        display(newQuantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+
+        // TextView is expecting a String.
+        quantityTextView.setText(String.valueOf(number));
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
